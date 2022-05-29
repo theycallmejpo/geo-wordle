77e649c76d40744c1ffa7ed22f3a2ca4ec8d322e
Juan Posadas <jposadasc@gmail.com>
2022-05-29 23:31:34 +0800

    made some progress with the model and rendering

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Events
 import Html exposing (Html, div, form, input, nav, p, text)
 import Html.Attributes as Attr exposing (class, type_, value)
 import Html.Events as Evts exposing (onClick)
@@ -14,7 +15,7 @@ main =
         { init = \_ -> ( Loading, fetchSolutionOfTheDay )
         , view = view
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = subscriptions
         }
 
 
@@ -34,18 +35,25 @@ type alias Solution =
 
 
 type Guess
-    = GuessIdle Int
+    = GuessIdle
     | GuessCurrent String
     | GuessFinished String Int
 
 
 
+-- list of guesses
+--  each guess has 5 letters (should be configurable)
+-- need to keep state about correctness of each letter? could probably be derived though
 -- UPDATE
 
 
 type Msg
     = GotResponse (Result Http.Error String)
     | CurrentGuessUpdated String
+    | LetterPressed Char
+    | BackspacePressed
+    | EnterPressed
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -54,7 +62,7 @@ update msg model =
         GotResponse res ->
             case res of
                 Ok solution ->
-                    ( LoadedInProgress [ GuessFinished "waaaz" 80, GuessCurrent "" ] solution 6
+                    ( LoadedInProgress [ GuessCurrent "" ] solution 6
                     , Cmd.none
                     )
 
@@ -80,6 +88,72 @@ update msg model =
 
                 _ ->
                     ( model, Cmd.none )
+
+        LetterPressed l ->
+            case model of
+                LoadedInProgress guesses solution maxGuesses ->
+                    case List.head <| List.reverse guesses of
+                        Just guess ->
+                            case guess of
+                                GuessCurrent currentGuess ->
+                                    let
+                                        updatedGuesses =
+                                            String.left 5 (currentGuess ++ String.fromChar l)
+                                                |> GuessCurrent
+                                                |> List.singleton
+                                                |> List.append
+                                                    (guesses
+                                                        |> List.reverse
+                                                        |> List.drop 1
+                                                        |> List.reverse
+                                                    )
+                                    in
+                                    ( LoadedInProgress updatedGuesses solution maxGuesses, Cmd.none )
+
+                                _ ->
+                                    ( model, Cmd.none )
+
+                        Nothing ->
+                            ( model, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        BackspacePressed ->
+            case model of
+                LoadedInProgress guesses solution maxGuesses ->
+                    case List.head <| List.reverse guesses of
+                        Just guess ->
+                            case guess of
+                                GuessCurrent currentGuess ->
+                                    let
+                                        updatedGuesses =
+                                            String.dropRight 1 currentGuess
+                                                |> GuessCurrent
+                                                |> List.singleton
+                                                |> List.append
+                                                    (guesses
+                                                        |> List.reverse
+                                                        |> List.drop 1
+                                                        |> List.reverse
+                                                    )
+                                    in
+                                    ( LoadedInProgress updatedGuesses solution maxGuesses, Cmd.none )
+
+                                _ ->
+                                    ( model, Cmd.none )
+
+                        Nothing ->
+                            ( model, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        EnterPressed ->
+            ( model, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
 
 
 
@@ -107,39 +181,74 @@ view model =
 
 viewHeader : Model -> Html msg
 viewHeader _ =
-    nav [ class "border-b w-full py-4 text-center font-bold text-2xl font-serif" ] [ text "Inca Kordle" ]
+    nav [ class "border-b w-full py-4 text-center font-bold text-2xl font-serif" ] [ text "Wordle" ]
 
 
 viewGame : List Guess -> Solution -> Int -> Html Msg
 viewGame guesses solution maxGuesses =
     createFillerGuesses (List.length guesses + 1) maxGuesses
         |> List.append guesses
-        |> List.map viewGuess
-        |> div [ class "mx-auto my-8 max-w-md text-sm" ]
+        |> Debug.log "guesses"
+        |> List.foldl viewGuess []
+        |> div [ class "mx-auto w-[24rem] my-8 text-sm grid grid-cols-5 grid-rows-6 gap-2" ]
 
 
-viewGuess : Guess -> Html Msg
-viewGuess guess =
+viewGuess : Guess -> List (Html Msg) -> List (Html Msg)
+viewGuess guess acc =
     case guess of
-        GuessIdle index ->
-            div
-                [ class "bg-slate-100 rounded-md px-4 py-2 w-full mb-4 cursor-not-allowed text-gray-300" ]
-                [ text <| "Intento #" ++ String.fromInt index ]
+        GuessIdle ->
+            List.map
+                (\_ -> div [ class "border border-black aspect-square flex justify-center items-center" ] [])
+                (List.repeat 5 0)
+                |> List.append acc
 
         GuessCurrent currentGuess ->
-            input
-                [ type_ "text"
-                , class "border rounded-md px-4 py-2 w-full mb-4"
-                , value currentGuess
-                , Attr.placeholder "Enter a guess"
-                , Evts.onInput CurrentGuessUpdated
-                ]
-                []
+            List.append (String.toList currentGuess) (List.repeat (5 - String.length currentGuess) ' ')
+                |> List.map
+                    (\letter ->
+                        div
+                            [ class "border border-black aspect-square flex justify-center items-center uppercase" ]
+                            [ text <| String.fromChar letter ]
+                    )
+                |> List.append acc
 
         GuessFinished userGuess _ ->
-            div
+            [ div
                 [ class "bg-green-200 rounded-md px-4 py-2 w-full mb-4" ]
                 [ text userGuess ]
+            ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Browser.Events.onKeyDown keyDecoder
+
+
+keyDecoder : Json.Decode.Decoder Msg
+keyDecoder =
+    Json.Decode.map toKey (Json.Decode.field "key" Json.Decode.string)
+
+
+toKey : String -> Msg
+toKey string =
+    case string of
+        "Backspace" ->
+            BackspacePressed
+
+        "Enter" ->
+            EnterPressed
+
+        _ ->
+            case String.uncons <| Debug.log "type" string of
+                Just ( letter, "" ) ->
+                    LetterPressed <| Debug.log "got" letter
+
+                _ ->
+                    NoOp
 
 
 
@@ -166,4 +275,4 @@ decoder =
 createFillerGuesses : Int -> Int -> List Guess
 createFillerGuesses min max =
     List.range min max
-        |> List.map (\index -> GuessIdle index)
+        |> List.map (\_ -> GuessIdle)
